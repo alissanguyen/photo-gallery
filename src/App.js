92e2e7e528133f9f6a1db645afe9a8e16b81af66
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2020-11-05 23:27:53 -0800

    Update README.md, add environment variables.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import logo from './galerie.svg';
 import './App.css';
 
+
+import Unsplash, { toJson } from 'unsplash-js';
+
+const unsplash = new Unsplash({
+  accessKey: APP_ACCESS_KEY,
+  // Optionally you can also configure a custom header to be sent with every request
+  headers: {
+    "X-Custom-Header": "foo"
+  },
+  // Optionally if using a node-fetch polyfill or a version of fetch which supports the timeout option, you can configure the request timeout for all requests
+  timeout: 500 // values set in ms
+});
+
 function App() {
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
